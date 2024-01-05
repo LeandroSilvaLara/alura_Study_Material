4241301ba3de806baba9c9eb4d80d713997ba008
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2024-01-04 22:39:19 -0300

    Criando Repositorio

---
+++ Rocketseat/gestao_vagas/src/main/java/br/com/leandrolara/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,8 @@
+package br.com.leandrolara.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+    
+}
