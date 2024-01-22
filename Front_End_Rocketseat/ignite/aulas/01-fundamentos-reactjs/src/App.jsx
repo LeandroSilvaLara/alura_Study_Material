e372172f627c526fcaf6e1687ce02e64a763243d
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2024-01-21 23:39:05 -0300

    Project Front

---
+++ Front_End_Rocketseat/ignite/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post.jsx";
+export function App() {
+  return (
+      <div>
+          <Post
+            author="Leandro Lara"
+            content="Lorem ispsun dolor sit amet consectetur adipiscing elite."
+          />
+          <Post
+              author="Gabriel Buzzi"
+              content="Um novo post muito legal"
+          />
+      </div>
+  )
+}
+
