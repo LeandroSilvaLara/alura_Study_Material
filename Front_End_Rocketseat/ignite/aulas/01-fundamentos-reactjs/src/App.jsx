ea21d273d5185df2b1b92edb2d2ce0836d2b9a7f
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2024-01-25 22:12:24 -0300

    CSS Modules

--- Front_End_Rocketseat/ignite/aulas/01-fundamentos-reactjs/src/App.jsx
+++ Front_End_Rocketseat/ignite/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,9 @@
+import { Header } from './components/Header';
 import { Post } from "./Post.jsx";
 export function App() {
   return (
       <div>
+          <Header />
           <Post
             author="Leandro Lara"
             content="Lorem ispsun dolor sit amet consectetur adipiscing elite."
